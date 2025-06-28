d2f819b8f6a26e2d934a785962481c06db2066d5
snow-jp-discover2025 <snowjpdiscover2025@gmail.com>
2025-06-28 09:54:49 +0900

    Add files via upload

---
+++ setup.sql
@@ -0,0 +1,48 @@
+// Step1: テーブル作成 //
+
+-- ロールの指定
+USE ROLE ACCOUNTADMIN;
+USE WAREHOUSE COMPUTE_WH;
+
+
+// Step2: 各種オブジェクトの作成 //
+
+-- データベースの作成
+CREATE OR REPLACE DATABASE SNOWRETAIL_DB;
+-- スキーマの作成
+CREATE OR REPLACE SCHEMA SNOWRETAIL_DB.SNOWRETAIL_SCHEMA;
+-- スキーマの指定
+USE SCHEMA SNOWRETAIL_DB.SNOWRETAIL_SCHEMA;
+
+-- ステージの作成
+CREATE OR REPLACE STAGE SNOWRETAIL_DB.SNOWRETAIL_SCHEMA.FILE DIRECTORY = (ENABLE = TRUE);
+CREATE OR REPLACE STAGE SNOWRETAIL_DB.SNOWRETAIL_SCHEMA.SEMANTIC_MODEL_STAGE DIRECTORY = (ENABLE = TRUE);
+
+
+// Step3: 公開されているGitからデータとスクリプトを取得 //
+
+-- Git連携のため、API統合を作成する
+CREATE OR REPLACE API INTEGRATION git_api_integration
+  API_PROVIDER = git_https_api
+  API_ALLOWED_PREFIXES = ('https://github.com/snow-jp-discover2025/')
+  ENABLED = TRUE;
+
+-- GIT統合の作成
+CREATE OR REPLACE GIT REPOSITORY GIT_INTEGRATION_FOR_HANDSON
+  API_INTEGRATION = git_api_integration
+  ORIGIN = 'https://github.com/snow-jp-discover2025/data-engineering.git';
+
+-- チェックする
+ls @GIT_INTEGRATION_FOR_HANDSON/branches/main;
+
+-- Githubからファイルを持ってくる
+COPY FILES INTO @SNOWRETAIL_DB.SNOWRETAIL_SCHEMA.FILE FROM @GIT_INTEGRATION_FOR_HANDSON/branches/main/data/;
+
+// Step4: Notebookを作成 //
+
+-- Notebookの作成
+CREATE OR REPLACE NOTEBOOK cortex_handson_part1
+    FROM @GIT_INTEGRATION_FOR_HANDSON/branches/main/handson1
+    MAIN_FILE = 'notebook.ipynb'
+    QUERY_WAREHOUSE = COMPUTE_WH
+    WAREHOUSE = COMPUTE_WH;
